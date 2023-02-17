fb0daeea1a5198f710936182a6dfb181c18b4a02
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-17 23:00:09 +0100

    Init project

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <div>
+      <h1>My First React </h1>
+      <p>It's a simple to-do app, with lists, columns and card</p>
+    </div>
+  );
+};
+export default Hero;
