3ba8fdec2a2d72799b9d1b871a0b049adf42bace
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-18 00:32:23 +0100

    Separate input component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,9 +1,10 @@
+import TextInput from "../TextInput/TextInput";
 import styles from "./SearchForm.module.scss";
 
 const SearchForm = () => {
   return (
     <form className={styles.form}>
-      <input type="text" className={styles.input} />
+      <TextInput placeholder="Search..." />
       <button className={styles.button}>Search</button>
     </form>
   );
