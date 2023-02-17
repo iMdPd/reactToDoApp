3d4eb931db3e5986935d8aff05bde5eedc322931
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-17 23:30:01 +0100

    Add List component and styles folder

---
+++ src/components/List/List.js
@@ -0,0 +1,22 @@
+const List = () => {
+  return (
+    <div>
+      <header>
+        THINGS TO DO <span>SOON</span>
+      </header>
+      <p>Interesting things I want to check out</p>
+      <section>
+        <article>
+          <h2>HTML</h2>
+        </article>
+        <article>
+          <h2>CSS</h2>
+        </article>
+        <article>
+          <h2>JavaScript</h2>
+        </article>
+      </section>
+    </div>
+  );
+};
+export default List;
