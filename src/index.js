7336c5dd60e8fdcd2abf92b68656601283e66a2a
Mariusz Politowicz <politowiczmar@gmail.com>
2023-02-28 19:34:00 +0100

    Create store file in redux folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 import App from "./App";
+
 import "./styles/normalize.scss";
 import "./styles/global.scss";
 import "font-awesome/css/font-awesome.min.css";
 
 const container = document.querySelector("#root");
 
-createRoot(container).render(<App />);
+createRoot(container).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
