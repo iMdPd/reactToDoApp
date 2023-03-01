b9b53cab266017b6bd3c3e571b02dcb8cf9d562b
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-01 21:06:36 +0100

    Add handleSubmit method in searchForm

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 import Button from "../Button/Button";
 import TextInput from "../TextInput/TextInput";
 import styles from "./SearchForm.module.scss";
 
 const SearchForm = () => {
+  const [title, setTitle] = useState("");
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault(e);
+    dispatch({ type: "UPDATE_COLUMNS" });
+  };
+
   return (
-    <form className={styles.searchForm}>
-      <TextInput placeholder={"Search..."} />
+    <form className={styles.searchForm} onSubmit={handleSubmit}>
+      <TextInput
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder={"Search..."}
+      />
       <Button>
         <span className="fa fa-search" />
       </Button>
