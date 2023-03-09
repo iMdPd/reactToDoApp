fbc166701ae78ca1fbc64352088660a823c4c8d5
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-09 15:40:13 +0100

    Add new attribute to InitialState object

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -39,5 +39,18 @@ export const initialState = {
     },
   ],
 
+  lists: [
+    {
+      id: 1,
+      title: "Things to do...",
+      description: "Interesting things I want to check out",
+    },
+    {
+      id: 2,
+      title: "Test list",
+      description: "Lorem Ipsum",
+    },
+  ],
+
   searchInput: "",
 };
