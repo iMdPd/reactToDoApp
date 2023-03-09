6939aec647ca6f1e0d9deb02974737f060b389cb
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-09 19:17:05 +0100

    Use Navigate component in List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,10 +2,10 @@ import styles from "./List.module.scss";
 import Column from "./../Column/Column";
 import { ColumnForm } from "./../ColumnForm/ColumnForm";
 import { SearchForm } from "../SearchForm/SearchForm";
-
 import { useSelector } from "react-redux";
 import { selectColumnsByList, selectListById } from "../../redux/store";
 import { useParams } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const List = () => {
   const { listId } = useParams();
@@ -15,6 +15,7 @@ export const List = () => {
     selectColumnsByList(state, listId)
   );
 
+  if (!listData) return <Navigate to="/" />;
   return (
     <div className={styles.list}>
       <header className={styles.header}>
