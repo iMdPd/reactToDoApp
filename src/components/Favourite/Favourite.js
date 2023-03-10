a8cf92532effca70d8fe7bc168ca600e86f74802
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-10 21:20:38 +0100

    Add  togle favouriteButton feature

--- src/components/Favourite/Favourite.js
+++ src/components/Favourite/Favourite.js
@@ -1,5 +1,4 @@
 import styles from "./Favourite.module.scss";
-
 import { PageTitle } from "../PageTitle/PageTitle";
 
 export const Favourite = () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -19,6 +19,10 @@ export const addColumn = (payload) => ({ type: "ADD_COLUMN", payload });
 export const addCard = (payload) => ({ type: "ADD_CARD", payload });
 export const addList = (payload) => ({ type: "ADD_LIST", payload });
 export const updateColumns = (payload) => ({ type: "UPDATE_COLUMNS", payload });
+export const toggleCardFavourite = (payload) => ({
+  type: "TOGGLE_CARD_FAVOURITE",
+  payload,
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -42,6 +46,16 @@ const reducer = (state, action) => {
 
     case "UPDATE_COLUMNS":
       return { ...state, searchInput: action.payload.title.toLowerCase() };
+
+    case "TOGGLE_CARD_FAVOURITE":
+      return {
+        ...state,
+        cards: state.cards.map((card) =>
+          card.id === action.payload
+            ? { ...card, isFavourite: !card.isFavourite }
+            : card
+        ),
+      };
 
     default:
       return state;
