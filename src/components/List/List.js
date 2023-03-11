04bba7da35caea10658cb60a46804c2d94fb9e07
Mariusz Politowicz <politowiczmar@gmail.com>
2023-03-11 15:29:37 +0100

    Update listsRedux imports

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,7 +3,8 @@ import Column from "./../Column/Column";
 import { ColumnForm } from "./../ColumnForm/ColumnForm";
 import { SearchForm } from "../SearchForm/SearchForm";
 import { useSelector } from "react-redux";
-import { selectColumnsByList, selectListById } from "../../redux/store";
+import { selectColumnsByList } from "../../redux/store";
+import { selectListById } from "../../redux/listsRedux";
 import { useParams } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
